f8b7263d29ad782595406223dfc88e6644d3157f
michaelangelovalente <miky10.mv@gmail.com>
2024-06-27 15:58:03 +0200

    SortedKeys and Navigable Map

---
+++ Advanced-java-features-17-21/CollectionFramework/SortedKeysAndNavigableMap.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class SortedKeysAndNavigableMap {
+
+    public static void main(String[] args) {
+
+        // #### Sorted maps
+        SortedMap<Integer, String> sMap = new TreeMap<>();
+        sMap.put(1, "one");
+        sMap.put(2, "two");
+        sMap.put(3, "three");
+        sMap.put(4, "four");
+        sMap.put(5, "five");
+        sMap.put(6, "six");
+
+        Integer viewUpTo = 3;
+        SortedMap<Integer, String> headMap = sMap.headMap(viewUpTo); // return view of map upTo val 3
+        headMap.put(0, "zero"); // works
+        // headMap.put(4, "four"); // does not work --> map is a view upTo 3
+
+        // #### NavigableMap
+        NavigableMap<Integer, String> nMap = new TreeMap<>();
+        nMap.put(1, "one");
+        nMap.put(2, "two");
+        nMap.put(3, "three");
+        nMap.put(4, "four");
+        nMap.put(5, "five");
+        nMap.put(6, "six");
+
+        nMap.keySet().forEach(k -> System.out.println(k + " "));
+        System.out.println();
+
+        NavigableSet<Integer> descindingKeys = nMap.descendingKeySet();
+        descindingKeys.forEach(k -> System.out.println(k + " "));
+
+    }
+}
